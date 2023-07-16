2ce1d504c67203f2efc1b00764a41d317ad93f13
KaminSirisu <Kaminsirisu@gmail.com>
2023-07-16 22:28:10 +0700

    update web

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
 const totalPriceElement = document.getElementById("totalPrice");
 const parentListElement = document.getElementById("list")
+
 let totalPrice = 0;
+let cart = [];
+
+class Item {
+    constructor(name, price) {
+        this.name = name;
+        this.price = price;
+    }
+}
+
+function refreshUI() {
+    totalPriceElement.innerText = `Total Price : $${totalPrice} | Total Items ${cart.length}`;
+    parentListElement.innerHTML = "";
+
+    cart.forEach((item, index) => {
+        console.log(cart);
+        const listElement = document.createElement("li");
+        const textNode = document.createTextNode(`${item.name} - $${item.price}`);
+        listElement.appendChild(textNode);
+        parentListElement.appendChild(listElement);
+        listElement.classList.add("list-group-item", "d-flex", "justify-content-between");
+
+        const deleteButton = document.createElement("button");
+        const deleteTextNode = document.createTextNode("delete");
+        deleteButton.appendChild(deleteTextNode);
+        deleteButton.classList.add("btn", "btn-danger");
+        listElement.appendChild(deleteButton);
+
+        deleteButton.addEventListener("click", () => {
+            cart.splice(index, 1);
+            totalPrice -= item.price;
+            refreshUI();
+        })
+    })
+}
 
 function addItem(form) {
     const itemName = form.itemName.value
@@ -13,27 +48,15 @@ function addItem(form) {
         alert("PLEASE PUT THE CORRECT NUMBER")
     }
 
-    console.log(parseInt(itemName));
     totalPrice += parseInt(itemPrice)
-    totalPriceElement.innerText = `Total Price : $${totalPrice}`;
+    const item = new Item(itemName, parseInt(itemPrice));
+    cart.push(item);
 
-    const listElement = document.createElement("li");
-    const textNode = document.createTextNode(`${itemName} - $${itemPrice}`);
-    listElement.appendChild(textNode);
-    parentListElement.appendChild(listElement);
-    listElement.classList.add("list-group-item", "d-flex", "justify-content-between");
+    localStorage.setItem("totalPrice", totalPrice);
+    localStorage.setItem("cart", cart);
 
-    const deleteButton = document.createElement("button");
-    const deleteTextNode = document.createTextNode("delete");
-    deleteButton.appendChild(deleteTextNode);
-    deleteButton.classList.add("btn", "btn-danger");
-    listElement.appendChild(deleteButton);
 
-    deleteButton.addEventListener("click", () => {
-        listElement.remove();
-        totalPrice -= parseInt(itemPrice);
-        totalPriceElement.innerText = `Total Price : $${totalPrice}`;
-    })
+    refreshUI();
 
 
     return false;
